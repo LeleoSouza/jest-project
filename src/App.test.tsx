1b0de525cbabbb369f13f635406f2e1fd72b1089
LeLeo <leobrausa@hotmail.com>
2021-10-26 00:13:08 -0400

    disable btn and test for it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { notDeepStrictEqual } from "assert";
 
 test("button has the correct intial color", () => {
   //  first thing render the repesctive file
@@ -17,4 +18,53 @@ test("button has the correct intial color", () => {
 
   // expect btn text to be "Change to red"
   expect(colorBtn.textContent).toBe("Change to red");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+  // check btn start enabled
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorBtn).toBeEnabled();
+  // check checkbox start out uncheck
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("btn should be disable when checkbox is checked & enabled when checkbox is unchecked", () => {
+  render(<App />);
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(colorBtn).toBeDisabled();
+
+  // uncheck checkbox
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+  expect(colorBtn).toBeEnabled();
+});
+
+test("Check the btn is gray when disable", () => {
+  render(<App />);
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(colorBtn).toBeDisabled();
+  expect(colorBtn).toHaveStyle({ backgroundColor: "gray" });
+});
+
+test("Change color, click disable and check for gray", () => {
+  render(<App />);
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  fireEvent.click(colorBtn);
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(colorBtn).toBeDisabled();
+  expect(colorBtn).toHaveStyle({ backgroundColor: "gray" });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import { useState } from "react";
 
 function App() {
   const [btnColor, setBtnColor] = useState("red");
-  const textBtn = btnColor === "red" ? "blue" : "red";
+  const [btnState, setBtnState] = useState(false);
+  const textColor = btnColor === "red" ? "blue" : "red";
+
+  const setColor = () => {
+    btnColor === "red" ? setBtnColor("blue") : setBtnColor("red");
+  };
+  const setDisable = () => {
+    setBtnState(!btnState);
+  };
+
   return (
     <div className='App'>
-      <button style={{ backgroundColor: btnColor }} onClick={() => setBtnColor(textBtn)}>
-        Change to {textBtn}
+      <button
+        style={{ backgroundColor: btnState ? "gray" : btnColor, color: "white" }}
+        onClick={() => setColor()}
+        disabled={btnState}
+      >
+        Change to {textColor}
       </button>
+      <input type='checkbox' id='enable-disable-checkbox' onClick={() => setDisable()} />
+      <label htmlFor='enable-disable-checkbox'>Disable button</label>
     </div>
   );
 }
